9a71ca3acf3fce4534dd8ed55618ee834763a7f2
NikolayO <n9056446438@gmail.com>
2025-10-08 18:31:58 +0300

    HW:12: CustomException added.

---
+++ src/main/java/org/skypro/skyshop/controller/ShopControllerAdvice.java
@@ -0,0 +1,19 @@
+package org.skypro.skyshop.controller;
+
+import org.skypro.skyshop.exception.NoSuchProductException;
+import org.skypro.skyshop.model.error.ShopError;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ShopControllerAdvice {
+
+    @ExceptionHandler(NoSuchProductException.class)
+    public ResponseEntity<ShopError> NoSuchProductHandler
+            (NoSuchProductException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new ShopError("NOT_FOUND_PRODUCT", "Продукт не найден"));
+    }
+}
